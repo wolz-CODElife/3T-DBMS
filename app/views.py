afd37b1fd9a242eb1247b97c2ac1932192910052
Adewole Joel <joeladewole3@gmail.com>
2021-01-05 13:00:12 +0100

    yet another debugging

--- app/views.py
+++ app/views.py
@@ -12,14 +12,7 @@ from datetime import datetime
 import csv
 import pandas as pd
 
-import numpy
-from psycopg2.extensions import register_adapter, AsIs
-def addapt_numpy_float64(numpy_float64):
-    return AsIs(numpy_float64)
-def addapt_numpy_int64(numpy_int64):
-    return AsIs(numpy_int64)
-register_adapter(numpy.float64, addapt_numpy_float64)
-register_adapter(numpy.int64, addapt_numpy_int64)
+
 static = os.path.join(app.root_path, 'static/')
 
 
